0f3fbe1852e70ac501bf733b48c354c1076ed005
Bjarte Johansen <bjarte.johansen@gmail.com>
2015-01-31 17:48:31 +0100

    gjør variabler lokale

--- src/main/webapp/js/move.js
+++ src/main/webapp/js/move.js
@@ -1,13 +1,13 @@
+d3.selectAll('rect').style('fill', 'gray');
+
 function updateVote(f, i) {
-    d3.selectAll('rect').style('fill', 'gray');
+    var rects = $('rect').sort(function(a,b) {
+        return parseInt(a.id) > parseInt(b.id) ? 1 : -1
+    });
 
-    rects = $('rect').sort(function(a,b) {
-        return parseInt(a.id) > parseInt(b.id) ? 1 : -1}
-                          );
-
-    forVote = d3.selectAll(rects.slice(0,f)).remove();
-    ikkeVote = d3.selectAll(rects.slice(f, i+f)).remove();
-    motVote = d3.selectAll(rects.slice(i+f,169)).remove();
+    var forVote = d3.selectAll(rects.slice(0,f)).remove();
+    var ikkeVote = d3.selectAll(rects.slice(f, i+f)).remove();
+    var motVote = d3.selectAll(rects.slice(i+f,169)).remove();
     d3.select('#layer1 > g').remove();
 
     d3.select('#layer1').insert('g').attr('id', 'forVote');
@@ -17,8 +17,6 @@ function updateVote(f, i) {
     $('#layer1 #forVote').append(forVote.map($));
     $('#layer1 #ikkeVote').append(ikkeVote.map($));
     $('#layer1 #motVote').append(motVote.map($));
-
-
 
     d3.selectAll('#forVote > rect')
         .transition()
