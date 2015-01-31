d466e0e61a02599ff2a4b8d63cc8f2aeba5b50e6
Bjarte Johansen <bjarte.johansen@gmail.com>
2015-01-31 17:31:21 +0100

    legg til ikke stemt

--- src/main/webapp/js/move.js
+++ src/main/webapp/js/move.js
@@ -1,27 +1,28 @@
-rects = $('rect').sort(function(a,b) {return parseInt(a.id) > parseInt(b.id) ? 1 : -1});
+rects = $('rect').sort(function(a,b) {
+return parseInt(a.id) > parseInt(b.id) ? 1 : -1}
+);
 
-trans1 = rects[99].transform;
-trans2 = rects[100].transform;
+f = 90;
+i = 40;
 
+forVote = d3.selectAll(rects.slice(0,f)).remove();
+ikkeVote = d3.selectAll(rects.slice(f, i+f)).remove();
+motVote = d3.selectAll(rects.slice(i+f,169)).remove();
 
-forVoteringer = d3.selectAll(rects.slice(0,100)).remove();
-motVoteringer = d3.selectAll(rects.slice(100)).remove();
+d3.select('#layer1').insert('g').attr('id', 'forVote');
+d3.select('#layer1').insert('g').attr('id', 'ikkeVote');
+d3.select('#layer1').insert('g').attr('id', 'motVote');
 
-d3.select('#layer1').insert('g').attr('id', 'forVoteringer');
-d3.select('#layer1').insert('g').attr('id', 'motVoteringer');
+$('#layer1 #forVote').append(forVote.map($));
+$('#layer1 #ikkeVote').append(ikkeVote.map($));
+$('#layer1 #motVote').append(motVote.map($));
 
-$('#layer1 #forVoteringer').append(forVoteringer.map($));
-$('#layer1 #motVoteringer').append(motVoteringer.map($));
-
-d3.select('#forVoteringer')
-    .transition()
-    .attr('transform', 'translate(-20)');
-d3.selectAll('#forVoteringer > rect')
+d3.selectAll('#forVote > rect')
     .transition()
     .style('fill', 'green');
-d3.select('#motVoteringer')
+d3.selectAll('#ikkeVote > rect')
     .transition()
-    .attr('transform', 'translate(20)');
-d3.selectAll('#motVoteringer > rect')
+    .style('fill', 'gray');
+d3.selectAll('#motVote > rect')
     .transition()
     .style('fill', 'red');
