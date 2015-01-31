a24bc194aa13d29118a090c03a5c62e5f3bcf502
Richard H. Olsen <einetunet@gmail.com>
2015-01-31 15:11:19 +0100

    Emnefactory

---
+++ src/main/java/net/feryla/webapptemplate/factory/EmneFactory.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.feryla.webapptemplate.factory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import net.feryla.webapptemplate.models.Emne;
+import net.feryla.webapptemplate.util.Downloader;
+
+/**
+ *
+ * @author clario
+ */
+public class EmneFactory {
+
+    private Map<String, Object> getEmneMap() throws IOException {
+
+        final String url = "http://data.stortinget.no/eksport/emner";
+
+        Downloader downloader = new Downloader();
+
+        String result = downloader.get(url);
+
+        HashMap<String, Object> objectMap = new ObjectMapper().readValue(result, HashMap.class);
+
+        return objectMap;
+
+    }
+
+    public List<Emne> getEmneList(Integer voteringId) throws IOException {
+        Map<String, Object> map = getEmneMap();
+
+        
+        
+        List<Emne> l = new ArrayList<>();
+
+        for (Map<String, Object> emne : (List<Map<String, Object>>) map.get("emne_liste")) {
+            l.add(convertEmne(emne));
+        }
+
+        return l;
+    }
+
+    private Emne convertEmne(Map<String, Object> emneMap) {
+
+        String emneNavn = (String) emneMap.get("navn");
+        Integer emneId = (Integer) emneMap.get("hovedemne_id");
+
+        Emne emne = new Emne(emneNavn, true, emneId);
+
+        return emne;
+
+    }
+
+}
